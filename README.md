037ae3e417905cdf28861902df6eb699f9bc1ed0
immno <dancermno@gmail.com>
2022-09-13 11:24:09 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# red-crab
+# red-crab(Financial Freedom Tool)
+[![CI status](https://github.com/immno/red-crab/actions/workflows/build.yml/badge.svg)](https://github.com/immno/red-crab/actions?query=workflow%3ABuild+branch%3Amain)
+[![Crates.io](https://img.shields.io/crates/v/ecbt.svg)](https://crates.io/crates/ecbt)
+[![Minimum rustc version](https://img.shields.io/badge/rustc-1.54+-lightgray.svg)](https://github.com/immno/red-crab)
+[![Join at Slack](https://img.shields.io/badge/chat-slack-brightgreen)](https://join.slack.com/t/wmjtyd/shared_invite/zt-17sfuaoj7-~0WmpuFL_NTqS~4WTNEXTg)
+
+Efficient and accurate financial tools.
+
+* Fundamental analysis.
+* Value factor analysis.
+
+## License
+
+[BSD 2-Clause License](https://opensource.org/licenses/BSD-2-Clause)
---
+++ src/prelude/shared.rs
@@ -0,0 +1,61 @@
+pub type Result<T> = std::result::Result<T, crate::prelude::errors::RedCrabError>;
+
+pub mod string_to_decimal {
+    use std::fmt;
+
+    use rust_decimal::prelude::*;
+    use serde::{de, Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<T, S>(value: &T, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        T: fmt::Display,
+        S: Serializer,
+    {
+        serializer.collect_str(value)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Decimal, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum StringToDecimal {
+            String(String),
+        }
+
+        let StringToDecimal::String(s) = StringToDecimal::deserialize(deserializer)?;
+        Decimal::from_str(&s).map_err(de::Error::custom)
+    }
+}
+
+pub mod string_to_opt_decimal {
+    use rust_decimal::prelude::*;
+    use serde::{Deserialize, Deserializer, Serializer};
+
+    pub fn serialize<S>(value: &Option<Decimal>, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        if let Some(value) = value {
+            return serializer.collect_str(&value);
+        }
+        serializer.serialize_none()
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Option<Decimal>, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        #[derive(Deserialize)]
+        #[serde(untagged)]
+        enum StringToOptDecimal {
+            String(Option<String>),
+        }
+        let StringToOptDecimal::String(s) = StringToOptDecimal::deserialize(deserializer)?;
+        if let Some(s) = s {
+            return Decimal::from_str(&s).map(Some).or(Ok(None));
+        }
+        Ok(None)
+    }
+}
